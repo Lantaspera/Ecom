4abf50bb2b77cbceb0dfabe063bf3f5ed3f9bd53
akshay pv <apvnambiar@gmail.com>
2023-03-19 23:30:34 +0530

    middleware added

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 import JWT from 'jsonwebtoken';
+import userModel from '../models/userModel.js';
 
 export const requireSignIn = async ( req, res, next ) => {
     try {
@@ -6,9 +7,33 @@ export const requireSignIn = async ( req, res, next ) => {
             req.headers.authorization,
             process.env.JWT_SECRET
         );
+        req.user = decode;
         next();
     }catch(error){
         console.log(error);
         
+    }
+}
+
+
+//admin access
+export const isAdmin = async (req, res, next) =>{
+    try {
+        const user = await userModel.findById(req.user._id)
+        if(!user.role !==1){
+            return res.status(401).send({
+                success:false,
+                message:"UnAuthorized Access"
+            })
+        }else{
+            next();
+        }
+       } catch (error) {
+        console.log()
+        res.status(401).send({
+            success:false,
+            error,
+            message:'Error in admin middleware'
+        })
     }
 }
