04159790fe61c520387082618f0443fc7bd5b1d4
akshay pv <apvnambiar@gmail.com>
2023-03-22 15:30:14 +0530

    react setup, components(header,footer,layout), pages(home,about,pnotfound), router dom

---
+++ client/src/pages/PagenotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/layout/Layout'
+
+const PagenotFound = () => {
+  return (
+    <Layout>
+        <h1>PagenotFound page</h1>
+    </Layout>
+    
+  )
+}
+
+export default PagenotFound
