9adf041725d517583eee2c1162e93d05a5ec4f0e
akshay pv <apvnambiar@gmail.com>
2023-03-23 19:03:17 +0530

    footer basic done

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import './footer.css'
 
 function Footer() {
   return (
-    <div className='bg-dark text-light p-3'>
+    <div className='footer bg-dark text-light p-3'>
         <h4 className='text-center'>Sketchgenz All right Reserved &copy; Lantaspera </h4>
+        <p  className=' text-center mt-3'>
+          <Link to='/about'>about |</Link>
+          <Link to='/contact'>Contact</Link>
+        </p>
     </div>
   )
 }
