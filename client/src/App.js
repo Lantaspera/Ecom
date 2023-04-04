09685acb4a137d16c2ae53515c414989eff0fe4a
akshay pv <apvnambiar@gmail.com>
2023-04-04 20:45:20 +0530

    Filter by price is functionality added

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import CreateCategory from './pages/admin/CreateCategory';
 import CreateProduct from './pages/admin/CreateProduct';
 import Users from './pages/admin/Users';
 import Products from './pages/admin/Products';
+import UpdateProduct from './pages/admin/UpdateProduct';
 
 
 function App() {
@@ -29,6 +30,7 @@ function App() {
           <Route path="admin" element={<AdminDashboard/>}/>
           <Route path="admin/create-category" element={<CreateCategory />} />
           <Route path="admin/create-product" element={<CreateProduct />} />
+          <Route path="admin/product/:slug" element={<UpdateProduct />} />
           <Route path="admin/products" element={<Products />} />
           <Route path="admin/users" element={<Users/>} />
         </Route>
