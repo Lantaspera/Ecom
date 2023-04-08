5576b8cd2b31eda3bbffd804bfe471324e5366e2
akshay pv <apvnambiar@gmail.com>
2023-04-09 00:32:34 +0530

    hooks add for the categorizing the product and cart page added

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,9 @@ import Orders from './user/Orders';
 import HomePage from './pages/HomePage';
 import Search from './pages/Search';
 import ProductDetails from './pages/ProductDetails';
+import Categories from './pages/Categories';
+import CategoryProduct from './pages/CategoryProduct';
+import CartPage from './pages/CartPage';
 
 function App() {
   return (
@@ -24,6 +27,9 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage/>} />
         <Route path="/product/:slug" element={<ProductDetails />} />
+        <Route path='/categories' element={<Categories/>}/>
+        <Route path='/category/:slug' element={<CategoryProduct/>}/>
+        <Route path='/cart' element={<CartPage/>}/>
         <Route path="/search" element={<Search/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/signup' element={<Signup/>}/>
--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -263,3 +263,49 @@ export const searchProductController = async (req, res) => {
     });
   }
 };
+
+// similar products
+export const relatedProductController = async (req, res) => {
+  try {
+    const { pid, cid } = req.params;
+    const products = await productModel
+      .find({
+        category: cid,
+        _id: { $ne: pid },
+      })
+      .select("-photo")
+      .limit(3)
+      .populate("category");
+    res.status(200).send({
+      success: true,
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      message: "error while geting related product",
+      error,
+    });
+  }
+};
+
+// get product by category
+export const productCategoryController = async (req, res) => {
+  try {
+    const category = await categoryModel.findOne({ slug: req.params.slug });
+    const products = await productModel.find({ category }).populate("category");
+    res.status(200).send({
+      success: true,
+      category,
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      error,
+      message: "Error While Getting products",
+    });
+  }
+};
